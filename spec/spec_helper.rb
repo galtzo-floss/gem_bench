a7b0912b35657333d2aa8234c5963e90256e0bc5
Peter H. Boling <peter.boling@gmail.com>
2026-08-05 04:31:56 -0600

    🐛 Keep debug loading out of CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 require "version_gem/ruby"
 
 # RSpec Configs
-require "debug" if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.7")
+require "debug" if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.7") && ENV["CI"].nil? && ENV.fetch("DEBUG", "false").casecmp("true").zero?
 require "config/rspec/rspec_block_is_expected"
 require "config/rspec/rspec_core"
 require "config/rspec/version_gem"
